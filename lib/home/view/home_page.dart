96d5d83d770ed1360013ca5c30585dedb3540180
Alisson Araujo <alissonaraujo@MacBook-Air-de-Alisson.local>
2024-01-29 23:20:12 -0300

    login functionality with bloc pattern

---
+++ lib/home/view/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:bloc_login/authentication/bloc/authentication_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  static Route<void> route() {
+    return MaterialPageRoute<void>(builder: (_) => const HomePage());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Builder(
+            builder: (context) {
+              final userId = context
+                  .select((AuthenticationBloc bloc) => bloc.state.user.id);
+              return Text('User id: $userId');
+            },
+          ),
+          ElevatedButton(
+            child: const Text('Logout'),
+            onPressed: () {
+              context
+                  .read<AuthenticationBloc>()
+                  .add(AuthenticationLogoutRequested());
+            },
+          )
+        ],
+      ),
+    ));
+  }
+}
